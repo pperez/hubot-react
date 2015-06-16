0d982341c140904d8bb685c9d6e9b6e4705a84a1
Patricio Pérez <pperez@boaboa.org>
2015-06-16 12:25:47 -0300

    CHG: Añadir log a la selección de stemmer
    Signed-off-by: Patricio Pérez <pperez@boaboa.org>

--- scripts/react.js
+++ scripts/react.js
@@ -34,8 +34,12 @@ var lang = process.env.HUBOT_REACT_STEMMER_LANG;
 if lang
     if _.isEqual(lang, 'spanish')
         stemmer = natural.PorterStemmerEs; // Set the PorterStemmer to spanish
+        console.log('Setting the stemmer to spanish')
     else if _.isEqual(lang, 'russian')
+        console.log('Setting the stemmer to english')
         stemmer = natural.PorterStemmerRu; // Set the PorterStemmer to russian
+    else
+        console.log('Unsupported language or english, the stemmer has been set to english')
 
 
 var STORE_SIZE = process.env.HUBOT_REACT_STORE_SIZE ? parseInt(process.env.HUBOT_REACT_STORE_SIZE) : 200;
